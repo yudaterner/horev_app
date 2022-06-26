8b381a36f3ef807bfc0c3bddf25435807c784f60
Your Name <you@example.com>
2022-06-26 11:03:27 +0300

    first commit

---
+++ telegram_boot/app.py
@@ -0,0 +1,26 @@
+import logging
+from telegram import Update
+from telegram.ext import filters, MessageHandler, ApplicationBuilder, CommandHandler, ContextTypes
+
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+
+if __name__ == '__main__':
+    application = ApplicationBuilder().token("2142884610:AAF4T21kIjCx8oeEDmexyalw1mImNDxCdFc").build()
+    
+    start_handler = CommandHandler('start', start)
+    echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
+    
+    application.add_handler(start_handler)
+    application.add_handler(echo_handler)
+    
+    application.run_polling()
---
+++ telegram_boot/credentials.py
@@ -0,0 +1,3 @@
+bot_token = "2142884610:AAF4T21kIjCx8oeEDmexyalw1mImNDxCdFc"
+bot_user_name = "horevsyberbot"
+URL = "the heroku app link that we will create later"
